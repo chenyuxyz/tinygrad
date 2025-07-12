62850f63e8625d2c89e8114497fa092abe2cdea1
Chen-Yu Yang <chenyu@fastmail.com>
2025-07-12 11:41:40 -0400

    simplify_ones only at the end of get_optimized_ast

--- tinygrad/opt/kernel.py
+++ tinygrad/opt/kernel.py
@@ -330,8 +330,6 @@ class Kernel:
       check(padded, "nothing was padded")
 
     if append_opt: self.applied_opts.append(opt)
-    if self.simplify_ones() and self.tensor_core_opts:
-      self.tensor_core_opts.fix_axes(axis) # fix up axes in TC opts if required after simplify_ones()
 
   def apply_opts(self, opts:Sequence[Opt]) -> Kernel:
     for opt in opts: self.apply_opt(opt)
