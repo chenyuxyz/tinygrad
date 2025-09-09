fbe8f9608d9da7c4849bf4f6ad43d9620b68d699
Chen-Yu Yang <chenyu@fastmail.com>
2025-09-08 23:02:29 -0400

    CACHELEVEL=0

--- tinygrad/runtime/support/compiler_cuda.py
+++ tinygrad/runtime/support/compiler_cuda.py
@@ -40,7 +40,7 @@ def cuda_disassemble(lib:bytes, arch:str):
 
 class CUDACompiler(Compiler):
   def __init__(self, arch:str, cache_key:str="cuda"):
-    self.arch, self.compile_options = arch, [f'--gpu-architecture={arch}', "--disable-warnings"]
+    self.arch, self.compile_options = arch, [f'--gpu-architecture={arch}']
     self.compile_options += [f"-I{CUDA_PATH}/include"] if CUDA_PATH else ["-I/usr/local/cuda/include", "-I/usr/include", "-I/opt/cuda/include"]
     nvrtc_check(nvrtc.nvrtcVersion((nvrtcMajor := ctypes.c_int()), (nvrtcMinor := ctypes.c_int())))
     if (nvrtcMajor.value, nvrtcMinor.value) >= (12, 4): self.compile_options.append("--minimal")
