2fc7eff763c69c51377ff7e57c6624c0c1dcd247
Chen-Yu Yang <chenyu@fastmail.com>
2025-11-30 12:56:12 -0500

    Renderer.extra_matcher has its own step [pr]
    AMD renderer has rules that are explicitly set to not be simplified away

--- tinygrad/codegen/__init__.py
+++ tinygrad/codegen/__init__.py
@@ -93,9 +93,9 @@ def full_rewrite_to_sink(sink:UOp, ren:Renderer|None=None, optimize:bool=True) -
   sink = graph_rewrite(sink, pm_decomp, ctx=ren.device, name="decompositions")
 
   # final rules for the renderer (without sym)
-  extra_matcher = ren.extra_matcher if ren.extra_matcher is not None else PatternMatcher([])
-  pm_final_rewrite = pm_decomp+pm_render+extra_matcher+pm_split_ends
+  pm_final_rewrite = pm_decomp+pm_render+pm_split_ends
   sink = graph_rewrite(sink, pm_final_rewrite, ctx=ren.device, name="final rewrite")
+  if ren.extra_matcher is not None: sink = graph_rewrite(sink, ren.extra_matcher, ctx=ren.device, name="renderer extra")
 
   # this was the linearizer
   sink = graph_rewrite(sink, pm_add_control_flow, ctx=CFGContext(sink), name="add control flow", bottom_up=True)
