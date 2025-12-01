86d0de7250db912c0f94e3eb5f0ba6144fc54a33
Chen-Yu Yang <chenyu@fastmail.com>
2025-11-30 21:34:46 -0500

    test rewrote rule [pr]

--- tinygrad/uop/symbolic.py
+++ tinygrad/uop/symbolic.py
@@ -31,8 +31,8 @@ propagate_invalid = PatternMatcher([
   (invalid_gate.cast(name="cast"), lambda i,x,cond,cast: x.cast(cast.dtype)),
   *((invalid_gate.alu(op, UPat.var("y")).named("alu"), lambda cond,x,y,alu,i: cond.where(x.alu(alu.op,y), i))
     for op in GroupOp.Binary-GroupOp.Comparison),
-  # TODO: when can this happen? and is it always safe to just drop invalid?
-  *((invalid_gate.alu(op, UPat.var("y")).named("alu"), lambda cond,x,y,alu,i: x.alu(alu.op,y)) for op in GroupOp.Comparison),
+  # # TODO: when can this happen? and is it always safe to just drop invalid?
+  # *((invalid_gate.alu(op, UPat.var("y")).named("alu"), lambda cond,x,y,alu,i: x.alu(alu.op,y)) for op in GroupOp.Comparison),
   # alu with invalid -> invalid
   *((invalid_pat.alu(op, UPat(dtype=dtypes.index)), lambda i: i) for op in GroupOp.Binary-GroupOp.Comparison),
 ])
