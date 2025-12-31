a24b7c0f0ad7991c0fb75493414349972af77d2c
Chen-Yu Yang <chenyu@fastmail.com>
2025-12-31 13:03:12 -0500

    when xor oob

--- tinygrad/uop/validate.py
+++ tinygrad/uop/validate.py
@@ -12,6 +12,7 @@ try:
   def z3_cdiv(a, b):return z3.If((a<0), z3.If(0<b, (a+(b-1))/b, (a-(b+1))/b), a/b)
   def z3_xor(a,b):
     if isinstance(a, z3.BoolRef): return a^b
+    raise
     assert a==-1 or b==-1, "xor can only be used in indexing if one of the arguments is -1"
     return -a-1 if b==-1 else -b-1
   z3_alu: dict[Ops, Callable] = python_alu | {Ops.MOD: lambda a,b: a-z3_cdiv(a,b)*b, Ops.IDIV: z3_cdiv, Ops.SHR: lambda a,b: a/(2**b.as_long()),
